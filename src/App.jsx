d65a216938b31f6f2928255a0a27e7c3652c3ecb
Acksout <157364563+Acksout@users.noreply.github.com>
2024-02-03 16:57:46 +0530

    final v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,134 @@
-import React from "react";
-import {useRef, useState} from "react";
+import React, { useState } from "react";
+import "./styles.css";
 
 export default function App() {
-    const inputRef = useRef(null)
-    const handleButtonClick =
-        (value) => {
-            if (inputRef.current) {
-                inputRef.current.value += value
-            }
+    const [inputContainer, setInputContainer] = useState("");
+    const handleButtonClick = (value) => {
+        setInputContainer((prevInputContainer) => prevInputContainer + value);
+    };
+
+    const resetButton = () => {
+        setInputContainer("");
+    };
+
+    const answerButton = () => {
+        try {
+            setInputContainer(eval(inputContainer).toString());
+        } catch (error) {
+            setInputContainer("Error");
         }
-    return (<div>
-        <h1>Calc</h1>
-        <input readOnly={false} ref={inputRef}/>
-        <div>
-            <button onClick={() => handleButtonClick("0")}>0</button>
-            <button onClick={() => handleButtonClick("1")}>1</button>
-            <button onClick={() => handleButtonClick("2")}>2</button>
-            <button onClick={() => handleButtonClick("3")}>3</button>
-            <button onClick={() => handleButtonClick("4")}>4</button>
-            <button onClick={() => handleButtonClick("5")}>5</button>
-            <button onClick={() => handleButtonClick("6")}>6</button>
-            <button onClick={() => handleButtonClick("7")}>7</button>
-            <button onClick={() => handleButtonClick("8")}>8</button>
-            <button onClick={() => handleButtonClick("9")}>9</button>
-            <button onClick={() => handleButtonClick("*")}>*</button>
-            <button onClick={() => handleButtonClick("/")}>/</button>
-            <button onClick={() => handleButtonClick("+")}>+</button>
-            <button onClick={() => handleButtonClick("-")}>-</button>
-            {/*This btn should give output*/}
-            {/*<button onClick={() => handleButtonClick("=")}>=</button>*/}
+    };
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-800 bg-opacity-80">
+            <div className="bg-gray-900 bg-opacity-30 p-8 rounded-md shadow-lg font-bold">
+                <h1 className="text-4xl text-center text-white mb-6">CALCULATOR</h1>
+                <div className="relative mb-4">
+                    <input
+                        type="text"
+                        readOnly={true}
+                        value={inputContainer}
+                        className="w-full bg-gray-800 text-white p-2 rounded-md focus:outline-none"
+                    />
+                </div>
+                <div className="grid grid-cols-4 gap-4">
+                    <button
+                        onClick={() => handleButtonClick("0")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        0
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("1")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        1
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("2")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        2
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("3")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        3
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("4")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        4
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("5")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        5
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("6")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        6
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("7")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        7
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("8")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        8
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("9")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        9
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("*")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        *
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("/")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        /
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("+")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        +
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("-")}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    >
+                        -
+                    </button>
+                    <button
+                        onClick={answerButton}
+                        className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 col-span-2"
+                    >
+                        =
+                    </button>
+                    <button
+                        onClick={resetButton}
+                        className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600 col-span-4"
+                    >
+                        CLEAR
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>)
+    );
 }
